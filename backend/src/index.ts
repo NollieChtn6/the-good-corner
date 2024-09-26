a7907383e6659462500b3b106ce8defea4aaa942
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-26 16:36:14 +0200

    delete type Ad

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import Express from "express";
 import sqlite3 from "sqlite3";
 import "reflect-metadata";
 import { dataSource } from "./config/db";
+import type { Ad } from "./entities/Ad";
 
 const app = Express();
 const PORT = 3000;
@@ -9,17 +10,6 @@ const PORT = 3000;
 const db = new sqlite3.Database("db.sqlite");
 
 app.use(Express.json());
-
-type Ad = {
-	id: number;
-	title: string;
-	description: string;
-	owner: string;
-	price: number;
-	picture: string;
-	location: string;
-	createdAt: string;
-};
 
 app.get("/", (req: Express.Request, res: Express.Response) => {
 	res.send("Hello World!");
@@ -52,40 +42,6 @@ app.get("/ads/:id", (req: Express.Request, res: Express.Response) => {
 app.post("/ads/create", (req: Express.Request, res: Express.Response) => {
 	const { title, description, owner, price, picture, location, createdAt } =
 		req.body as Ad;
-
-	const newAd: Omit<Ad, "id"> = {
-		title,
-		description,
-		owner,
-		price,
-		picture,
-		location,
-		createdAt,
-	};
-
-	db.run(
-		// sql
-		"INSERT INTO ads ('title', 'description', 'owner', 'price', 'picture', 'location', 'createdAt') VALUES (?, ?, ?, ?, ?, ?, ?)",
-		// parameter
-		[
-			req.body.title,
-			req.body.description,
-			req.body.owner,
-			req.body.price,
-			req.body.picture,
-			req.body.location,
-			req.body.createdAt,
-		],
-		// callback
-		(err) => {
-			if (err) {
-				console.log(err);
-				return res.status(500).send(err);
-			}
-			console.log(err);
-			res.send("Request received, check the backend terminal");
-		},
-	);
 });
 
 app.delete("/ads/:id/delete", (req: Express.Request, res: Express.Response) => {
