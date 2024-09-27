1769cd327376fd6694d794c1e02fc36a46e5eb22
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-27 17:02:04 +0200

    run first migration

---
+++ backend/src/migrations/1727446838992-initialize.ts
@@ -0,0 +1,7 @@
+import type { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Initialize1727446838992 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {}
+}
