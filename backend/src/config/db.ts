5cdf88a883ac30feacfa6a73d554557fb9a3a770
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-29 14:30:23 +0200

    import Category entity and configure migrations

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,12 @@
 import { DataSource } from "typeorm";
 import { Ad } from "../entities/Ad";
+import { Category } from "../entities/Category";
 
 export const dataSource = new DataSource({
 	type: "sqlite",
 	database: "./the-good-corner.sqlite",
-	entities: [Ad],
-	synchronize: true,
+	entities: [Ad, Category],
+	synchronize: false,
+	migrations: ["./src/migrations/*.ts"],
+	migrationsTableName: "migrations",
 });
