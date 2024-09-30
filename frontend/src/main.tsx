e1dbd30c829d8a7b55f80b8b9c72f44940eb56eb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-09-30 11:14:25 +0200

    fix: 'forbidden non null assertion'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// https://www.codingdeft.com/posts/react-18-typescript-error/
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
