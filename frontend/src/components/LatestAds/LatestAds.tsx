54a75b916bb3ef3e10190a3cc4cdffd35729bd8f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 12:19:31 +0200

    create button and method to increase cart total

--- frontend/src/components/LatestAds/LatestAds.tsx
+++ frontend/src/components/LatestAds/LatestAds.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AdCard from "../AdCard/AdCard";
 
 import ads from "../../data/ads.json";
@@ -5,20 +6,38 @@ import ads from "../../data/ads.json";
 import "../../index.css";
 
 function LatestAds() {
+	const [cartTotal, setCartTotal] = useState<number>(0);
+
+	const handleAddToCart = (price: number) => {
+		setCartTotal(cartTotal + price);
+	};
+
 	return (
 		<>
 			<h2>Annonces récentes</h2>
+			<p>Total du panier&nbsp;: {cartTotal}&nbsp;€</p>
+
 			<section className="recent-ads">
 				{ads.map((ad) => (
-					<AdCard
-						id={ad.id}
-						title={ad.title}
-						price={ad.price}
-						img={ad.img}
-						alt={ad.alt}
-						link={ad.link}
-						key={ad.id}
-					/>
+					<div key={ad.title}>
+						<AdCard
+							id={ad.id}
+							title={ad.title}
+							price={ad.price}
+							img={ad.img}
+							alt={ad.alt}
+							link={ad.link}
+							key={ad.id}
+						/>
+						<button
+							type="button"
+							key=""
+							className="button"
+							onClick={() => handleAddToCart(ad.price)}
+						>
+							Ajouter au panier
+						</button>
+					</div>
 				))}
 			</section>
 		</>
