edf00eae3fff8c21a6f7afedb23f76f285aacc14
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-01 16:31:58 +0200

    adjust data according to data structure from backend

--- frontend/src/components/AdCard/AdCard.tsx
+++ frontend/src/components/AdCard/AdCard.tsx
@@ -4,16 +4,18 @@ type AdCardProps = {
 	id: number;
 	title: string;
 	price: number;
-	img: string;
-	alt: string;
-	link: string;
+	picture: string;
 };
 
-function AdCard({ id, title, price, img, alt, link }: AdCardProps) {
+function AdCard({ id, title, price, picture }: AdCardProps) {
 	return (
 		<div className="ad-card-container">
-			<a className="ad-card-link" href={link} id={`object_${id}`}>
-				<img className="ad-card-image" src={img} alt={alt} />
+			<a className="ad-card-link" href={`/ads/${id}`} id={`object_${id}`}>
+				<img
+					className="ad-card-image"
+					src={picture}
+					alt={`Une représentation visuelle de ${title}`}
+				/>
 				<div className="ad-card-text">
 					<div className="ad-card-title">{title}</div>
 					<div className="ad-card-price">{price}&nbsp;€</div>
