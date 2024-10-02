64e273e799018a86532f9779a5d3e2d3d9be8290
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-02 12:00:56 +0200

    create basic form (not dynamic)

--- frontend/src/components/NewAdForm/NewAdForm.tsx
+++ frontend/src/components/NewAdForm/NewAdForm.tsx
@@ -1,5 +1,31 @@
+import type { FormEvent } from "react";
+
 function NewAdForm() {
-	return <p>We'll create a new ad on this page</p>;
+	const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const form = e.target;
+		const formData = new FormData(form as HTMLFormElement);
+		console.log(formData);
+	};
+
+	return (
+		<form onSubmit={handleFormSubmit}>
+			<label>
+				Titre de l&rsquo;annonce&nbsp;:
+				<br />
+				<input className="text-field" name="title" />
+			</label>
+			<br />
+			<label>
+				Prix de vente&nbsp;:
+				<br />
+				<input className="text-field" name="price" />
+			</label>
+			<button className="button" type="submit">
+				Créer
+			</button>
+		</form>
+	);
 }
 
 export default NewAdForm;
