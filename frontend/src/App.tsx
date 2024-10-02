b98fef45d9e56d3d8995ebf2b0fab22ea95e2c1d
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-02 17:07:53 +0200

    fetch data from ads and categories store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
+
 import Layout from "./components/Layout/Layout";
+import { store } from "./store/storeIndex";
 
 import "./App.css";
 
 function App() {
+	const fetchAds = store.adsStore((state) => state.fetchAds);
+	const fetchCategories = store.categoriesStore(
+		(state) => state.fetchCategories,
+	);
+
+	useEffect(() => {
+		const loadData = async () => {
+			await fetchAds();
+			await fetchCategories();
+		};
+		loadData();
+	}, [fetchAds, fetchCategories]);
+
 	return <Layout />;
 }
 
