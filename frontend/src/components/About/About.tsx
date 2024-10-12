50b6c9e2672a1114e98698d6397e93dbc26ebe26
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-12 22:25:28 +0200

    delete About page

--- frontend/src/components/About/About.tsx
+++
@@ -1,5 +0,0 @@
-function About() {
-	return <p>This project was made with React and ViteJS ❤️ </p>;
-}
-
-export default About;
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
 import LatestAds from "../components/LatestAds/LatestAds";
-import About from "../components/About/About";
 import AdDetails from "../components/AdDetails/AdDetails";
 import NewAdForm from "../components/NewAdForm/NewAdForm";
 
@@ -22,10 +21,6 @@ const router = createBrowserRouter([
 			{
 				path: "/ads/create",
 				element: <NewAdForm />,
-			},
-			{
-				path: "/about",
-				element: <About />,
 			},
 		],
 	},
