40a98a93280a510cee7647a162ef51f71cd0b8e1
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-13 18:44:57 +0200

    display the last 9 ads on landing page and create button to access all ads

--- frontend/src/pages/LatestAds.tsx
+++ frontend/src/pages/LatestAds.tsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import AdCard from "../components/AdCard";
-
 import { store } from "../store/storeIndex";
 
 function LatestAds() {
-	const [cartTotal, setCartTotal] = useState<number>(0);
 	const ads = store.adsStore((state) => state.ads);
-	console.log(ads);
-
-	const handleAddToCart = (price: number) => {
-		setCartTotal(cartTotal + price);
-	};
+	const latestAds = ads.slice(Math.max(ads.length - 9, 0));
 
 	return (
 		<>
 			<h2>Annonces récentes</h2>
 			<section className="recent-ads">
-				{ads.map((ad) => (
+				{latestAds.map((ad) => (
 					<div key={ad.title}>
 						<AdCard
 							id={ad.id}
@@ -24,18 +18,16 @@ function LatestAds() {
 							price={ad.price}
 							pictureUrl={ad.pictureUrl}
 							key={ad.id}
+							location={ad.location}
 						/>
-						<button
-							type="button"
-							key=""
-							className="button"
-							onClick={() => handleAddToCart(ad.price)}
-						>
-							Ajouter au panier
-						</button>
 					</div>
 				))}
 			</section>
+			<div className="view-more-container">
+				<Link to="/annonces" className="button button-primary">
+					Voir toutes les annonces
+				</Link>
+			</div>
 		</>
 	);
 }
