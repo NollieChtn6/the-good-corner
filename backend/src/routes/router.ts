71955480d851d3cd102596246e98e2a30c4f54d9
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-13 18:56:57 +0200

    import tagsRouter into global router

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -2,6 +2,8 @@ export const router = require("express").Router();
 
 import { adsRouter } from "./adsRouter";
 import { categoriesRouter } from "./categoriesRouter";
+import { tagsRouter } from "./tagsRouter";
 
 router.use(adsRouter);
 router.use(categoriesRouter);
+router.use(tagsRouter);
