e0f28c20cc194dbbd18d22518e129c66ede29896
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-13 18:32:33 +0200

    import tags store into global store

--- frontend/src/store/storeIndex.ts
+++ frontend/src/store/storeIndex.ts
@@ -1,7 +1,9 @@
 import { useAdsStore } from "./adsStore";
 import { useCategoriesStore } from "./categoriesStore";
+import { useTagsStore } from "./tagsStore";
 
 export const store = {
 	adsStore: useAdsStore,
 	categoriesStore: useCategoriesStore,
+	tagsStore: useTagsStore,
 };
