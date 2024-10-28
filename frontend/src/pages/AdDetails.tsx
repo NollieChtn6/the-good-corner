71202f11033667bbb72ec55e74f4b82caaa05cf9
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 07:49:48 +0100

    import EditAdForm and handle ad editing's logic on click

--- frontend/src/pages/AdDetails.tsx
+++ frontend/src/pages/AdDetails.tsx
@@ -3,20 +3,32 @@ import { DateTime } from "luxon";
 import { store } from "../store/storeIndex";
 import TagItem from "../components/TagItem";
 import { ArrowLeft, Pencil, MapPin, Trash2 } from "lucide-react";
+import EditAdForm from "../components/EditAdForm";
+import { useState } from "react";
+import type { UpdateAdFormData } from "../@types/types";
 
 const AdDetails = () => {
 	const navigate = useNavigate();
 	const { id } = useParams<{ id: string }>();
-	const ad = store.adsStore((state) =>
-		state.ads.find((ad) => ad.id === Number(id)),
-	);
-	if (!ad) {
-		return <p>Annonce non trouvée</p>;
-	}
+	const [editFormIsVisible, setEditFormIsVisible] = useState<boolean>(false);
+
+	const ad = store.adsStore.getState().ads.find((ad) => ad.id === Number(id));
+	const updateAd = store.adsStore.getState().updateAd;
+	if (!ad) return <p>Annonce non trouvée</p>;
 
 	const formattedCreationDate = DateTime.fromISO(ad.createdAt).toLocaleString(
 		DateTime.DATE_FULL,
 	);
+
+	const handleSave = async (updatedAd: UpdateAdFormData) => {
+		console.log(
+			"Mise à jour de l'annonce avec les données suivantes :",
+			updatedAd,
+		);
+		console.log("ID de l'annonce :", ad.id);
+		await updateAd(ad.id, updatedAd);
+		setEditFormIsVisible(false);
+	};
 
 	return (
 		<>
@@ -49,16 +61,26 @@ const AdDetails = () => {
 				<p className="ad-details-price">{ad.price}&nbsp;€</p>
 				Annonce ajoutée le&nbsp;: {formattedCreationDate}
 				<div className="ad-details-actions">
-					<button className="button" type="button">
-						<Pencil size={16} className="icon edit-icon" />
-						Modifier
+					<button
+						className="button"
+						onClick={() => setEditFormIsVisible(true)}
+						type="button"
+					>
+						<Pencil size={16} className="icon edit-icon" /> Modifier
 					</button>
 					<button className="button" type="button">
-						<Trash2 size={16} className="icon delete-icon" />
-						Supprimer
+						<Trash2 size={16} className="icon delete-icon" /> Supprimer
 					</button>
 				</div>
 			</div>
+			{editFormIsVisible && (
+				<EditAdForm
+					isVisible={editFormIsVisible}
+					selectedAd={ad}
+					onClose={() => setEditFormIsVisible(false)}
+					onSave={handleSave}
+				/>
+			)}
 		</>
 	);
 };
