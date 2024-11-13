8689896484654fcd67eb48d910bfcac1402d71cf
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-13 16:56:18 +0100

    remove Express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,4 @@
-import Express from "express";
 import "reflect-metadata";
-import { catchValidationErrors } from "./middlewares/errorHandlerMiddleware";
 import { dataSource } from "./config/db";
-import { router } from "./routes/router";
 
-import cors from "cors";
-
-const app = Express();
-const PORT = 3000;
-
-app.use(cors());
-app.use(Express.json());
-app.use(catchValidationErrors);
-app.use("/api", router);
-
-app.listen(PORT, async () => {
-	await dataSource.initialize();
-	console.log(`App listening on: http://localhost:${PORT}`);
-});
+await dataSource.initialize();
