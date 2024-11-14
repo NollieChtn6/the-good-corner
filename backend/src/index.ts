10dab190f4d5d673c009654e82abf5555fdd98a6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 09:58:39 +0100

    build Apollo server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import "reflect-metadata";
-import { dataSource } from "./config/db";
+import { buildSchema } from "type-graphql";
+import { initializeDataSource } from "./config/db";
+import { AdResolver } from "./resolvers/AdResolver";
 
-await dataSource.initialize();
+const PORT = 4000;
+
+const startServer = async () => {
+  await initializeDataSource();
+  const schema = await buildSchema({
+    resolvers: [AdResolver],
+  });
+  const server = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: PORT },
+  });
+
+  console.log(`Server ready at ${url}`);
+};
+
+startServer();
