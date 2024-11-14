42d29d611c5c45fd6e0679b07d54221ef9677d4c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 09:26:35 +0100

    create Query 'ads' in AdResolver

--- backend/src/resolvers/AdResolver.ts
+++ backend/src/resolvers/AdResolver.ts
@@ -1,23 +1,32 @@
-import { Arg, Field, InputType, Mutation, Query, Resolver, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import { AdEntity } from "../entities/Ad";
 
 @InputType()
 class AdInput {
-	@Field()
-	title!: string;
+  @Field()
+  title!: string;
 
-	@Field()
-	description!: string;
+  @Field()
+  description!: string;
 
-	@Field()
-	owner!: string;
+  @Field()
+  owner!: string;
 
-	@Field()
-	price!: number;
+  @Field()
+  price!: number;
 
-	@Field()
-	pictureUrl?: string;
+  @Field()
+  pictureUrl?: string;
 
-	@Field()
-	location!: string;
+  @Field()
+  location!: string;
+}
+
+@Resolver(AdEntity)
+export class AdResolver {
+  @Query(() => [AdEntity])
+  async ads() {
+    const ads = await AdEntity.find();
+    return ads;
+  }
 }
