4645e7608c6dd062d30f1858ed0bc8529e6a80ab
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 14:11:37 +0100

    add @Field decorator to CategoryEntity

--- backend/src/entities/Category.ts
+++ backend/src/entities/Category.ts
@@ -1,23 +1,21 @@
-import {
-	BaseEntity,
-	Column,
-	Entity,
-	OneToMany,
-	PrimaryGeneratedColumn,
-} from "typeorm";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
 import { AdEntity } from "./Ad";
 
 @Entity({ name: "category" })
+@ObjectType()
 export class CategoryEntity extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	id!: number;
+  @PrimaryGeneratedColumn()
+  @Field()
+  id!: number;
 
-	@Column({ length: 200 })
-	name!: string;
+  @Column({ length: 200 })
+  @Field()
+  name!: string;
 
-	@OneToMany(
-		() => AdEntity,
-		(ad) => ad.category,
-	)
-	ads!: AdEntity[];
+  @OneToMany(
+    () => AdEntity,
+    (ad) => ad.category,
+  )
+  ads!: AdEntity[];
 }
