3a3defce817850a29423696364b991012fae49d4
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 14:11:57 +0100

    add @Field decorator to TagEntity

--- backend/src/entities/Tag.ts
+++ backend/src/entities/Tag.ts
@@ -5,14 +5,18 @@ import {
 	PrimaryGeneratedColumn,
 	ManyToMany,
 } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
 import { AdEntity } from "./Ad";
 
 @Entity({ name: "tag" })
+@ObjectType()
 export class TagEntity extends BaseEntity {
 	@PrimaryGeneratedColumn()
+	@Field()
 	id!: number;
 
 	@Column({ length: 200 })
+	@Field()
 	label!: string;
 
 	@ManyToMany(
