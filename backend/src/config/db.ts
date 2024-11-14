265963585d318113bf85e14ca85434643afcee9f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 09:50:49 +0100

    create 'initializeDataSource' function

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,11 +3,20 @@ import { AdEntity } from "../entities/Ad";
 import { CategoryEntity } from "../entities/Category";
 import { TagEntity } from "../entities/Tag";
 
-export const dataSource = new DataSource({
-	type: "sqlite",
-	database: "./the-good-corner.sqlite",
-	entities: [AdEntity, CategoryEntity, TagEntity],
-	synchronize: true,
-	// migrations: ["./src/migrations/*.ts"],
-	// migrationsTableName: "migrations",
+const dataSource = new DataSource({
+  type: "sqlite",
+  database: "./the-good-corner.sqlite",
+  entities: [AdEntity, CategoryEntity, TagEntity],
+  synchronize: true,
 });
+
+export const initializeDataSource = async () => {
+  dataSource
+    .initialize()
+    .then(() => {
+      console.log("Data Source has been initialized successfully");
+    })
+    .catch((error) => {
+      console.log("Error during Data Source initialization", error);
+    });
+};
