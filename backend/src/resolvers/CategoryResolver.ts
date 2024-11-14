3b324dce7011e5ffe8557ebc389fd82e63a9665f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-14 14:30:19 +0100

    create categoryById query in CategoryResolver

--- backend/src/resolvers/CategoryResolver.ts
+++ backend/src/resolvers/CategoryResolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver, Arg } from "type-graphql";
 import { CategoryEntity } from "../entities/Category";
 
 @Resolver(CategoryEntity)
@@ -7,5 +7,14 @@ export class CategoryResolver {
   async categories(): Promise<CategoryEntity[]> {
     const categories = await CategoryEntity.find();
     return categories;
+  }
+
+  @Query(() => CategoryEntity)
+  async categortById(@Arg("id") id: number): Promise<CategoryEntity> {
+    const selectedCategory = await CategoryEntity.findOneByOrFail({ id });
+    if (!selectedCategory) {
+      throw new Error("Category not found!");
+    }
+    return selectedCategory;
   }
 }
