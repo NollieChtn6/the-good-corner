c51e7e7c8adf86888140f975e67f75128c144f9f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-15 14:02:21 +0100

    create and configure ApolloClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "https://flyby-router-demo.herokuapp.com/",
+
+	cache: new InMemoryCache(),
+});
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
 const rootElement = document.getElementById("root");
-
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
 root.render(
-	<React.StrictMode>
+	<ApolloProvider client={client}>
 		<RouterProvider router={router} />
-	</React.StrictMode>,
+	</ApolloProvider>,
 );
