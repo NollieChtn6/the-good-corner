02cb95c6f44823a8f75bb99afa4526bb149c2848
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 20:09:11 +0100

    include tags to Ads and AdById queries

--- frontend/src/graphql/adQueries.ts
+++ frontend/src/graphql/adQueries.ts
@@ -2,42 +2,50 @@ import type { ResultOf } from "gql.tada";
 import { graphql } from "../graphql";
 
 export const ADS_QUERY = graphql(`
-query AdsQuery {
-  ads {
-    id
-    title
-    description
-    owner
-    price
-    pictureUrl
-    location
-    createdAt
-    updatedAt
-    category {
+  query AdsQuery {
+    ads {
       id
-      name
+      title
+      description
+      owner
+      price
+      pictureUrl
+      location
+      createdAt
+      updatedAt
+      category {
+        id
+        name
+      }
+      tags {
+        id
+        label
+      }
     }
   }
-}
 `);
 
 export type AdsQueryResult = ResultOf<typeof ADS_QUERY>;
 
 export const AD_BY_ID_QUERY = graphql(`
-  query AdById($adByIdId: Float!) {
-  adById(id: $adByIdId) {
-    id
-    title
-    description
-    owner
-    price
-    pictureUrl
-    location
-    createdAt
-    updatedAt
-    category {
-      id
-      name
-    }
-  }
-}`);
+    query AdById($adByIdId: Float!) {
+      adById(id: $adByIdId) {
+        id
+        title
+        description
+        owner
+        price
+        pictureUrl
+        location
+        createdAt
+        updatedAt
+        category {
+          id
+          name
+        }
+        tags {
+          id
+          label
+        }
+      }
+    }`);
