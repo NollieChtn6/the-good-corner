734e46045baa654200508d6a217c7513750c0c6e
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 15:37:02 +0100

    update page layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
 function Layout() {
-	return (
-		<main className="main-content">
-			<Header />
-			<Outlet />
-		</main>
-	);
+  return (
+    <main className="main-content">
+      <Header />
+      <div className="outlet-container">
+        <Outlet />
+      </div>
+    </main>
+  );
 }
 
 export default Layout;
