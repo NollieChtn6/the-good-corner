6d1e321c75e0b9d21a24200a1f9abae51f2337dc
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 14:12:11 +0100

    remove functions fetching  data from Zustand stores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,9 @@
-import { useEffect } from "react";
-
 import Layout from "./pages/Layout";
-import { store } from "./store/storeIndex";
-
 import "./App.css";
 import "./index.css";
 
 function App() {
-	const fetchAds = store.adsStore((state) => state.fetchAds);
-	const fetchCategories = store.categoriesStore(
-		(state) => state.fetchCategories,
-	);
-	const fetchTags = store.tagsStore((state) => state.fetchTags);
-
-	useEffect(() => {
-		const loadData = async () => {
-			await fetchAds();
-			await fetchCategories();
-			await fetchTags();
-		};
-		loadData();
-	}, [fetchAds, fetchCategories, fetchTags]);
-
-	return <Layout />;
+  return <Layout />;
 }
 
 export default App;
