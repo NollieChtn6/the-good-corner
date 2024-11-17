59fe53a6e2423cdb3c5e6cebc893137e14351af8
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 19:58:44 +0100

    add @Field() decorator to ManyToMany relation with AdEntity in TagEntity

--- backend/src/entities/Tag.ts
+++ backend/src/entities/Tag.ts
@@ -1,27 +1,22 @@
-import {
-	BaseEntity,
-	Column,
-	Entity,
-	PrimaryGeneratedColumn,
-	ManyToMany,
-} from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToMany } from "typeorm";
 import { ObjectType, Field } from "type-graphql";
 import { AdEntity } from "./Ad";
 
 @Entity({ name: "tag" })
 @ObjectType()
 export class TagEntity extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	@Field()
-	id!: number;
+  @PrimaryGeneratedColumn()
+  @Field()
+  id!: number;
 
-	@Column({ length: 200 })
-	@Field()
-	label!: string;
+  @Column({ length: 200 })
+  @Field()
+  label!: string;
 
-	@ManyToMany(
-		() => AdEntity,
-		(ad) => ad.tags,
-	)
-	ads!: AdEntity[];
+  @Field(() => [AdEntity])
+  @ManyToMany(
+    () => AdEntity,
+    (ad) => ad.tags,
+  )
+  ads!: AdEntity[];
 }
