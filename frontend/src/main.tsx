360c2c20140264dad2b30bb61547ea32c5a5da07
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 14:08:07 +0100

    create and configure ApolloClient

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,8 @@ import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: "https://flyby-router-demo.herokuapp.com/",
-
-	cache: new InMemoryCache(),
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
 });
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
@@ -15,7 +14,7 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(
-	<ApolloProvider client={client}>
-		<RouterProvider router={router} />
-	</ApolloProvider>,
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>,
 );
