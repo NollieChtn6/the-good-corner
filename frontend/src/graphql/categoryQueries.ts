50627f2858bcd383f6dafa1fcd8108eaaec3c4b6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 17:55:47 +0100

    reindent file

--- frontend/src/graphql/categoryQueries.ts
+++ frontend/src/graphql/categoryQueries.ts
@@ -2,21 +2,21 @@ import type { ResultOf } from "gql.tada";
 import { graphql } from "../graphql";
 
 export const CATEGORIES_QUERY = graphql(`
-query Categories {
-  categories {
-    id
-    name
+  query Categories {
+    categories {
+      id
+      name
+    }
   }
-}
 `);
 
 export type CategoriesQueryResult = ResultOf<typeof CATEGORIES_QUERY>;
 
 export const CATEGORY_BY_ID_QUERY = graphql(`
-query CategoryById($categoryByIdId: Float!) {
-  categoryById(id: $categoryByIdId) {
-    id
-    name
-  }
-}
+    query CategoryById($categoryByIdId: Float!) {
+      categoryById(id: $categoryByIdId) {
+        id
+        name
+      }
+    }
 `);
