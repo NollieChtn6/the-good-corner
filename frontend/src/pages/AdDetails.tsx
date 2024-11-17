9adb05ac6bbb9a7121eb9be0a2537df8363a8678
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 16:54:51 +0100

    fetch details of selected ad using AD_BY_ID_QUERY and update page layout structure

--- frontend/src/pages/AdDetails.tsx
+++ frontend/src/pages/AdDetails.tsx
@@ -1,88 +1,87 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { DateTime } from "luxon";
-import { store } from "../store/storeIndex";
-import TagItem from "../components/TagItem";
+import type { Ad } from "../@types/types";
 import { ArrowLeft, Pencil, MapPin, Trash2 } from "lucide-react";
-import EditAdForm from "../components/EditAdForm";
-import { useState } from "react";
-import type { UpdateAdFormData } from "../@types/types";
+import { AD_BY_ID_QUERY } from "../graphql/adQueries";
+import { useQuery } from "@apollo/client";
 
 const AdDetails = () => {
-	const navigate = useNavigate();
-	const { id } = useParams<{ id: string }>();
-	const [editFormIsVisible, setEditFormIsVisible] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const { data, loading, error } = useQuery<{ adById: Ad }>(AD_BY_ID_QUERY, {
+    variables: { adByIdId: Number(id) },
+  });
 
-	const ad = store.adsStore.getState().ads.find((ad) => ad.id === Number(id));
-	const updateAd = store.adsStore.getState().updateAd;
-	if (!ad) return <p>Annonce non trouvée</p>;
+  const ad = data?.adById;
 
-	const formattedCreationDate = DateTime.fromISO(ad.createdAt).toLocaleString(
-		DateTime.DATE_FULL,
-	);
+  // const [editFormIsVisible, setEditFormIsVisible] = useState<boolean>(false);
 
-	const handleSave = async (updatedAd: UpdateAdFormData) => {
-		console.log(
-			"Mise à jour de l'annonce avec les données suivantes :",
-			updatedAd,
-		);
-		console.log("ID de l'annonce :", ad.id);
-		await updateAd(ad.id, updatedAd);
-		setEditFormIsVisible(false);
-	};
+  if (!ad) return <p>Annonce non trouvée</p>;
+  if (loading) return <p>Chargement de l&rsquo;annonce en cours...</p>;
+  if (error) return <p>Une erreur est survenue lors du chargement de l&rsquo;annonce.</p>;
 
-	return (
-		<>
-			<button
-				className="button back-btn"
-				onClick={() => navigate(-1)}
-				type="button"
-			>
-				<ArrowLeft /> Retour
-			</button>
-			<div className="ad-details">
-				<h1>{ad.title}</h1>
-				<div className="ad-details-meta">
-					<p>
-						Annonce publiée par <strong>{ad.owner}</strong> dans{" "}
-						<strong>{ad.category.name}</strong>
-					</p>
-					<p>
-						<MapPin />
-						{ad.location}
-					</p>
-				</div>
-				<img className="ad-details-image" src={ad.pictureUrl} alt={ad.title} />
-				<p>{ad.description}</p>
-				<div className="tags-container">
-					{ad.tags.map((tag) => (
-						<TagItem key={tag.id} tag={tag} />
-					))}
-				</div>
-				<p className="ad-details-price">{ad.price}&nbsp;€</p>
-				Annonce ajoutée le&nbsp;: {formattedCreationDate}
-				<div className="ad-details-actions">
-					<button
-						className="button"
-						onClick={() => setEditFormIsVisible(true)}
-						type="button"
-					>
-						<Pencil size={16} className="icon edit-icon" /> Modifier
-					</button>
-					<button className="button" type="button">
-						<Trash2 size={16} className="icon delete-icon" /> Supprimer
-					</button>
-				</div>
-			</div>
-			{editFormIsVisible && (
-				<EditAdForm
-					isVisible={editFormIsVisible}
-					selectedAd={ad}
-					onClose={() => setEditFormIsVisible(false)}
-					onSave={handleSave}
-				/>
-			)}
-		</>
-	);
+  const formattedCreationDate = DateTime.fromISO(ad.createdAt).toLocaleString(DateTime.DATE_FULL);
+
+  // const handleSave = async (updatedAd: UpdateAdFormData) => {
+  //   console.log("Mise à jour de l'annonce avec les données suivantes :", updatedAd);
+  //   console.log("ID de l'annonce :", ad.id);
+  //   await updateAd(ad.id, updatedAd);
+  //   setEditFormIsVisible(false);
+  // };
+
+  return (
+    <div className="page-content">
+      <div className="btn-container">
+        <button className="button back-btn" onClick={() => navigate(-1)} type="button">
+          <ArrowLeft /> Retour
+        </button>
+      </div>
+      <div className="ad-details-card">
+        <h1 className="ad-title">{ad.title}</h1>
+        <div className="ad-details-meta">
+          <p className="ad-owner">
+            Annonce publiée par <strong>{ad.owner}</strong> dans <strong>{ad.category.name}</strong>
+          </p>
+          <p className="ad-location">
+            <MapPin />
+            {ad.location}
+          </p>
+        </div>
+        <div className="ad-details-image-container">
+          <img className="ad-details-image" src={ad.pictureUrl} alt={ad.title} />
+        </div>
+        <div className="ad-details-description-container">
+          <p className="ad-description">{ad.description}</p>
+        </div>
+        {/* <div className="tags-container">
+          {ad.tags.map((tag) => (
+            <TagItem key={tag.id} tag={tag} />
+          ))}
+        </div> */}
+
+        <div className="ad-details-meta">
+          <p className="ad-price">{ad.price}&nbsp;€</p>
+          <p className="ad-timestamp">Annonce ajoutée le&nbsp;: {formattedCreationDate} </p>
+        </div>
+        <div className="ad-details-actions">
+          <button className="button" type="button">
+            <Pencil size={16} className="icon edit-icon" /> Modifier
+          </button>
+          <button className="button" type="button">
+            <Trash2 size={16} className="icon delete-icon" /> Supprimer
+          </button>
+        </div>
+      </div>
+      {/* {editFormIsVisible && (
+        <EditAdForm
+          isVisible={editFormIsVisible}
+          selectedAd={ad}
+          onClose={() => setEditFormIsVisible(false)}
+          onSave={handleSave}
+        />
+      )} */}
+    </div>
+  );
 };
 
 export default AdDetails;
