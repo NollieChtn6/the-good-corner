5b4cd0b83d0ed3d6b0c244446cdf4b3893346f26
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 19:17:59 +0100

    fetch ads by category and update page layout to display ads in a gallery

--- frontend/src/pages/Category.tsx
+++ frontend/src/pages/Category.tsx
@@ -1,7 +1,48 @@
+import { useQuery } from "@apollo/client";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import type { Ad } from "../@types/types";
+import AdCard from "../components/AdCard";
+import { ADS_BY_CATEGORY_QUERY } from "../graphql/categoryQueries";
+
 export function Category() {
+  const [message, setMessage] = useState<string | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const { data, loading, error } = useQuery<{ adsByCategory: Ad[] }>(ADS_BY_CATEGORY_QUERY, {
+    variables: { categoryId: Number(id) },
+  });
+  const ads = data?.adsByCategory ?? [];
+
+  if (loading) {
+    return <div>Chargement des annonces en cours...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Une erreur est survenue pendant le chargement des annonces.</div>;
+  }
+  if (ads.length === 0) {
+    setMessage("Aucune annonce trouvée pour cette catégorie.");
+  }
+
   return (
-    <div>
-      <h2>Toutes les annonces de la catégorie</h2>
+    <div className="page-content">
+      <h2>Annonces de la catégorie Test</h2>
+      {message ? (
+        <p>{message}</p>
+      ) : (
+        <section className="gallery">
+          {ads.map((ad) => (
+            <AdCard
+              key={ad.id}
+              id={ad.id}
+              title={ad.title}
+              price={ad.price}
+              pictureUrl={ad.pictureUrl}
+              location={ad.location}
+            />
+          ))}
+        </section>
+      )}
     </div>
   );
 }
