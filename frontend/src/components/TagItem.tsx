397fffc16a40891c5ce1e1270ab031a640cd48c9
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 20:03:58 +0100

    turn TagItem into function

--- frontend/src/components/TagItem.tsx
+++ frontend/src/components/TagItem.tsx
@@ -1,15 +1,13 @@
 import type { Tag } from "../@types/types";
 
 type TagsProps = {
-	tag: Tag;
+  tag: Tag;
 };
 
-const TagItem = ({ tag }: TagsProps) => {
-	return (
-		<div className="tag-item" id={`tag-${tag.id}`}>
-			{tag.label}
-		</div>
-	);
-};
-
-export default TagItem;
+export function TagItem({ tag }: TagsProps) {
+  return (
+    <div className="tag-item" id={`tag-${tag.id}`}>
+      {tag.label}
+    </div>
+  );
+}
