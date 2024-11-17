a67cd8758573d40eee24aa5117043cddcd65b23a
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-17 17:48:15 +0100

    create route '/categories/:id'

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,34 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "../App";
-import LatestAds from "../pages/LatestAds";
 import AdDetails from "../pages/AdDetails";
-import CreateNewAd from "../pages/CreateNewAd";
 import AllAds from "../pages/Ads";
+import { Category } from "../pages/Category";
+import CreateNewAd from "../pages/CreateNewAd";
+import LatestAds from "../pages/LatestAds";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				index: true,
-				element: <LatestAds />,
-			},
-			{
-				path: "/annonces/",
-				element: <AllAds />,
-			},
-			{
-				path: "/annonces/:id",
-				element: <AdDetails />,
-			},
-			{
-				path: "/annonces/nouvelle-annonce",
-				element: <CreateNewAd />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LatestAds />,
+      },
+      {
+        path: "/annonces/",
+        element: <AllAds />,
+      },
+      {
+        path: "/annonces/:id",
+        element: <AdDetails />,
+      },
+      {
+        path: "/annonces/nouvelle-annonce",
+        element: <CreateNewAd />,
+      },
+      {
+        path: "/categories/:id",
+        element: <Category />,
+      },
+    ],
+  },
 ]);
 
 export default router;
