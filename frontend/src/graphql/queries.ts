8a978964867e5afbfa1b6bb3d4720c7e0c29bbc3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-18 10:23:59 +0100

    gather queries into the same file

--- frontend/src/graphql/queries.ts
+++ frontend/src/graphql/queries.ts
@@ -1,6 +1,55 @@
 import type { ResultOf } from "gql.tada";
 import { graphql } from "../graphql";
 
+export const ADS_QUERY = graphql(`
+  query AdsQuery {
+    ads {
+      id
+      title
+      description
+      owner
+      price
+      pictureUrl
+      location
+      createdAt
+      updatedAt
+      category {
+        id
+        name
+      }
+      tags {
+        id
+        label
+      }
+    }
+  }
+`);
+
+export type AdsQueryResult = ResultOf<typeof ADS_QUERY>;
+
+export const AD_BY_ID_QUERY = graphql(`
+    query AdById($adByIdId: Float!) {
+      adById(id: $adByIdId) {
+        id
+        title
+        description
+        owner
+        price
+        pictureUrl
+        location
+        createdAt
+        updatedAt
+        category {
+          id
+          name
+        }
+        tags {
+          id
+          label
+        }
+      }
+    }`);
+
 export const CATEGORIES_QUERY = graphql(`
   query Categories {
     categories {
