b6c1b096222f25d000b81a101ff79313ed309bf6
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-19 12:25:14 +0100

    create ReplaceAdById mutation

--- frontend/src/graphql/mutations.ts
+++ frontend/src/graphql/mutations.ts
@@ -23,3 +23,25 @@ export const CREATE_AD_MUTATION = graphql(`
   }`);
 
 export type CreateAdMutationResult = ResultOf<typeof CREATE_AD_MUTATION>;
+
+export const UPDATE_AD_MUTATION = graphql(`
+  mutation ReplaceAdById($data: AdInput!, $replaceAdByIdId: Float!) {
+    replaceAdById(data: $data, id: $replaceAdByIdId) {
+      id
+      title
+      description
+      owner
+      price
+      pictureUrl
+      location
+      createdAt
+      updatedAt
+      category {
+        name
+      }
+      tags {
+        label
+      }
+    }
+  }
+`);
