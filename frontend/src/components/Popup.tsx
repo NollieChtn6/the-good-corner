22c0ecfdd8559f1325b0246d9a6385fb1fc5c413
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-19 13:43:06 +0100

    handle ad deletion using DeleteAd mutation

--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -1,5 +1,8 @@
+import { useMutation } from "@apollo/client";
 import type { MouseEvent } from "react";
 import type { Ad } from "../@types/types";
+import { DELETE_AD_MUTATION } from "../graphql/mutations";
+import { useNavigate } from "react-router-dom";
 
 type EditAdFormProps = {
   isVisible: boolean;
@@ -8,9 +11,22 @@ type EditAdFormProps = {
 };
 
 export function Popup({ selectedAd, onClose }: EditAdFormProps) {
+  const navigate = useNavigate();
+
+  const [deleteAd, { error: deleteAdError }] = useMutation(DELETE_AD_MUTATION);
+
   const handleDeleteAd = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log("Ad Id:", selectedAd.id);
+    try {
+      const response = await deleteAd({
+        variables: { deleteAdId: selectedAd.id },
+      });
+      console.log(response.data?.deleteAd);
+      navigate("/");
+    } catch (error) {
+      console.log(deleteAdError);
+      console.error("Failed to delete ad:", error);
+    }
   };
 
   return (
