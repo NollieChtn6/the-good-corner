2bbbaa05bd3e824f8482d021f5b263d5e10e138c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-19 15:10:54 +0100

    update data type 'datetime' to 'timestamp' as it is not supported in psql

--- backend/src/entities/Ad.ts
+++ backend/src/entities/Ad.ts
@@ -1,12 +1,12 @@
 import {
-  BaseEntity,
-  Column,
-  Entity,
-  ManyToOne,
-  ManyToMany,
-  JoinTable,
-  JoinColumn,
-  PrimaryGeneratedColumn,
+	BaseEntity,
+	Column,
+	Entity,
+	ManyToOne,
+	ManyToMany,
+	JoinTable,
+	JoinColumn,
+	PrimaryGeneratedColumn,
 } from "typeorm";
 
 import { ObjectType, Field } from "type-graphql";
@@ -17,62 +17,62 @@ import { TagEntity } from "./Tag";
 @Entity({ name: "ad" })
 @ObjectType()
 export class AdEntity extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  @Field()
-  id!: number;
+	@PrimaryGeneratedColumn()
+	@Field()
+	id!: number;
 
-  @Column({ length: 200 })
-  @Field()
-  title!: string;
+	@Column({ length: 200 })
+	@Field()
+	title!: string;
 
-  @Column()
-  @Field()
-  description!: string;
+	@Column()
+	@Field()
+	description!: string;
 
-  @Column()
-  @Field()
-  owner!: string;
+	@Column()
+	@Field()
+	owner!: string;
 
-  @Column()
-  @Field()
-  price!: number;
+	@Column()
+	@Field()
+	price!: number;
 
-  @Column()
-  @Field()
-  pictureUrl?: string;
+	@Column()
+	@Field()
+	pictureUrl?: string;
 
-  @Column()
-  @Field()
-  location!: string;
+	@Column()
+	@Field()
+	location!: string;
 
-  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
-  @Field()
-  createdAt: Date = new Date();
+	@Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+	@Field()
+	createdAt: Date = new Date();
 
-  @Column({ type: "datetime", nullable: true })
-  @Field({ nullable: true })
-  updatedAt?: Date;
+	@Column({ type: "timestamp", nullable: true })
+	@Field({ nullable: true })
+	updatedAt?: Date;
 
-  @Field(() => CategoryEntity)
-  @JoinColumn({ name: "category" })
-  @ManyToOne(
-    () => CategoryEntity,
-    (category) => category.ads,
-  )
-  category!: CategoryEntity;
+	@Field(() => CategoryEntity)
+	@JoinColumn({ name: "category" })
+	@ManyToOne(
+		() => CategoryEntity,
+		(category) => category.ads,
+	)
+	category!: CategoryEntity;
 
-  @Field(() => [TagEntity])
-  @ManyToMany(() => TagEntity, { cascade: true })
-  @JoinTable({
-    name: "ad_has_tags",
-    joinColumn: {
-      name: "adId",
-      referencedColumnName: "id",
-    },
-    inverseJoinColumn: {
-      name: "tagId",
-      referencedColumnName: "id",
-    },
-  })
-  tags!: TagEntity[];
+	@Field(() => [TagEntity])
+	@ManyToMany(() => TagEntity, { cascade: true })
+	@JoinTable({
+		name: "ad_has_tags",
+		joinColumn: {
+			name: "adId",
+			referencedColumnName: "id",
+		},
+		inverseJoinColumn: {
+			name: "tagId",
+			referencedColumnName: "id",
+		},
+	})
+	tags!: TagEntity[];
 }
