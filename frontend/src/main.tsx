b7d57a3306b237b4954dfda7b1360a8be3af701f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-20 09:11:14 +0100

    update url to backend api

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import router from "./router/router";
 import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+const { VITE_GATEWAY_PORT } = import.meta.env;
+
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
+	uri: `http://localhost:${VITE_GATEWAY_PORT}/api`,
+	cache: new InMemoryCache(),
 });
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
@@ -14,7 +16,7 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
 root.render(
-  <ApolloProvider client={client}>
-    <RouterProvider router={router} />
-  </ApolloProvider>,
+	<ApolloProvider client={client}>
+		<RouterProvider router={router} />
+	</ApolloProvider>,
 );
