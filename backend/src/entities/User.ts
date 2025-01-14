6e7b58b60994b6ddbe7e1b94223b394deaaa52eb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-01-14 17:11:24 +0100

    remove unused imports

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,32 +1,23 @@
-import {
-	BaseEntity,
-	Column,
-	Entity,
-	ManyToOne,
-	ManyToMany,
-	JoinTable,
-	JoinColumn,
-	PrimaryGeneratedColumn,
-} from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 import { ObjectType, Field } from "type-graphql";
 
 @Entity({ name: "user" })
 @ObjectType()
 export class UserEntity extends BaseEntity {
-	@PrimaryGeneratedColumn()
-	@Field()
-	id!: number;
+  @PrimaryGeneratedColumn()
+  @Field()
+  id!: number;
 
-	@Column({ unique: true })
-	@Field()
-	username!: string;
+  @Column({ unique: true })
+  @Field()
+  username!: string;
 
-	@Column({ unique: true, nullable: false })
-	@Field()
-	email!: string;
+  @Column({ unique: true, nullable: false })
+  @Field()
+  email!: string;
 
-	@Column()
-	@Field()
-	password!: string;
+  @Column()
+  @Field()
+  password!: string;
 }
