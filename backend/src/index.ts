5567f30f42f35aa8411cc79de5f6743432944097
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-01-15 17:00:08 +0100

    protect routes with authChecker()

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,20 +11,41 @@ import { UserResolver } from "./resolvers/UserResolver";
 const PORT = 4000;
 
 const startServer = async () => {
-	await initializeDataSource();
-	const schema = await buildSchema({
-		resolvers: [AdResolver, CategoryResolver, TagResolver, UserResolver],
-	});
-	const server = new ApolloServer({ schema });
-	const { url } = await startStandaloneServer(server, {
-		listen: { port: PORT },
-		context: async ({ res }) => {
-			console.log("Server has received request");
-			return { res };
-		},
-	});
+  await initializeDataSource();
+  const schema = await buildSchema({
+    resolvers: [AdResolver, CategoryResolver, TagResolver, UserResolver],
+    authChecker: ({ context }, authorizedRoles) => {
+      // No one can access protected resources
+      // return false;
+      // Everyone can access protected resources
+      // return true;
+      /* 
+				Only connected users can access protected resources.
+				Even if all users have a defined role in database, 
+				'role' is included in the token content *only* after registration/login.
+			 */
+      // if (!context.user.role) {
+      //   return false;
+      // }
+      // return true;
 
-	console.log(`Server ready at ${url}`);
+      // Only users with the roles defined in 'authorizedRoles' can access protected resources
+      if (!authorizedRoles.includes(context.user?.role)) {
+        return false;
+      }
+      return true;
+    },
+  });
+  const server = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: PORT },
+    context: async ({ res }) => {
+      // console.log("Server has received request");
+      return { res };
+    },
+  });
+
+  console.log(`Server ready at ${url}`);
 };
 
 startServer();
