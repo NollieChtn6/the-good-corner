ddd8b577b2f99f6f0f9dd582efa9fc6cd6a41170
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-01-16 13:19:55 +0100

    update tada-gql schema and output

--- frontend/src/graphql.ts
+++ frontend/src/graphql.ts
@@ -2,14 +2,14 @@ import { initGraphQLTada } from "gql.tada";
 import type { introspection } from "./graphql-env";
 
 export const graphql = initGraphQLTada<{
-  introspection: introspection;
-  scalars: {
-    DateTimeISO: Date;
-    DateTime: Date;
-    Float: number;
-    String: string;
-    ID: number;
-  };
+	introspection: introspection;
+	scalars: {
+		DateTimeISO: Date;
+		DateTime: Date;
+		Float: number;
+		String: string;
+		ID: number;
+	};
 }>();
 
 export type { FragmentOf, ResultOf, VariablesOf } from "gql.tada";
