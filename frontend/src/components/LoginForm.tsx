b4d0f0b4f359c3ae8219d5ce228ea1b10eed5b4f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-01-16 13:50:27 +0100

    update LoginForm content

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,3 +1,31 @@
 export function LoginForm() {
-  return "This is my login form";
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  return (
+    <div className="form-container">
+      <h1 className="form-title">Se connecter</h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <div className="form-input">
+          <label className="label" htmlFor="email">
+            Adresse e-mail&nbsp;:
+          </label>
+          <input className="text-field" name="email" id="email" required />
+        </div>
+        <div className="form-input">
+          <label className="label" htmlFor="password">
+            Mot de passe&nbsp;:
+          </label>
+          <input className="text-field" type="password" name="password" id="password" required />
+        </div>
+        <div className="btn-container">
+          <button className="button" type="submit">
+            Se connecter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
